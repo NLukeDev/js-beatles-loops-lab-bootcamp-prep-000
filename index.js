fcaa5531b56ee937272f5e039f894746e842cc8e
Nekwan Luke <contact@nekwanluke.com>
2019-04-16 23:15:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,22 +19,6 @@ function johnLennonFacts() {
     return newFacts;
 }
 
-describe('johnLennonFacts', function(){
-    it("returns an array of strings with exclamation points", function() {
-      expect(johnLennonFacts([
-        "He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-        "He hated the sound of his own voice"
-      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
-
-      expect(johnLennonFacts([
-        "foo",
-        "bar",
-      ])).toEqual(["foo!!!", "bar!!!"])
-    });
-  });
-
 function iLoveTheBeatles (num) {
   var love = [];
   
